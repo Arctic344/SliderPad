cb55289d62d5dfbff120ac80e9e88b8f35a47455
Arctic344 <arctic344.spam@gmail.com>
2024-05-14 20:48:09 +0930

    added linear slider functionality

---
+++ src/main-linearSliderTest.cpp
@@ -0,0 +1,63 @@
+#include <Arduino.h>
+#include <Adafruit_PWMServoDriver.h>
+#include "sliderclasses/linearslider.h"
+#include <Wire.h>
+#include "Updater.h"
+
+// The following sets up the driver for the Motors
+Adafruit_PWMServoDriver driver(0x60);
+
+IC_MC14051 *mux1ptr = new IC_MC14051(9,21,20,19);
+
+MC14051_Potentiometer p1(1,mux1ptr);
+
+Capacitivetouch t1(1);
+
+Motor m1(&driver,1);
+
+// set up the slider
+LinearSlider slider1(&m1,&p1,&t1);
+
+Updater u1; 
+
+
+
+
+
+
+void setup() {
+  u1.addNode(&t1);
+  u1.addNode(&p1);
+  // The following sets up debug system and prints a message to check if initiated
+  Serial.begin(115200);
+  delay(500);
+  Serial.println("Hello world");
+
+  // The following sets up the I2C communication ports
+  Wire.setPins(11,12);
+
+  // The following sets up the Motor Driver/s and sets all the pins to 0 to reset the device
+  driver.begin();
+  for (int i = 1; i < 16; i++) {
+    driver.setPin(1,0);
+  }
+  Serial.println("calibrating Slider");
+  slider1.calibrate();
+  Serial.println("Calibration complete");
+}
+
+int n = 0;
+#include <cstdlib> // Include the library for generating random numbers
+
+void loop() {
+  u1.update_Components();
+  slider1.update_Device();
+  n++;
+  if (n > 800) {
+    int goal = rand() % 101; // Generate a random number between 0 and 100
+    slider1.set_goal(goal);
+    Serial.print("Set goal to: ");
+    Serial.println(goal);
+    n = 0;
+  }
+}
--- src/sliderclasses/linearslider.h
+++ src/sliderclasses/linearslider.h
@@ -2,5 +2,50 @@
 #include "../Slider.h"
 
 class LinearSlider: public Slider{
+    private:
+        int speedToTarget;
+        int goal;
+    public:
+        LinearSlider(Motor* motor, Potentiometer* pot, Capacitivetouch* touch) {
+            this->motor = motor;
+            this->pot = pot;
+            this->touch = touch;
+            speedToTarget = 30;
+            goal = -100;
+        }
 
+        void update_Device() override {
+            if (goal == -100 || touch->get_touchBool()) {
+                goal = -100;
+                motor->setSpeed(0);
+                return;
+            }
+            if (abs(get_position() - goal) < 3) {
+                motor->setSpeed(0);
+                goal = -100;
+            } else if (get_position() - goal > 0) {
+                motor->setSpeed(speedToTarget);
+            } else {
+                motor->setSpeed(speedToTarget*-1);
+            }
+        }
+
+        int get_position() override {
+            return pot->get_potValue();
+        }
+
+        void set_speedToTarget(int value) {
+            speedToTarget = 30+value;
+        }
+
+        void set_goal(int value) {
+            if (value >= 0 && value <= 100) {
+                goal = value;
+            } else {
+                // Handle invalid goal value here (e.g. throw an exception or set a default value)
+            }
+        }
+        void remove_goal() {
+            goal = -100;
+        }
 };
