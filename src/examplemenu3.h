3ca7a955bbe431c3b29fafcee6d2b9f819508f41
Arctic344 <arctic344.spam@gmail.com>
2024-05-21 23:04:33 +0930

    added example 3

--- src/examplemenu3.h
+++ src/examplemenu3.h
@@ -2,17 +2,36 @@
 #include "Menu.h"
 #include "AdafruitIO_WiFi.h"
 
+
 class Example3 : public Menu {
     private:
+        AdafruitIO_WiFi io;
+        AdafruitIO_Feed *SliderPadTestValue;
     public:
-    Example3() : Menu(1) {
-
+    Example3() : Menu(1), io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS) {
     }
     public:
+    void on_setup() override{
+        AdafruitIO_Feed *SliderPadTestValue = io.feed("SliderPadTestValue");
+        Serial.println("Example 3 setup");
+        Serial.print("Connecting to Adafruit IO");
+        io.connect();
+        while (io.status() < AIO_CONNECTED) {
+            Serial.print(".");
+            delay(500);
+        }
+    }
+
+
     bool is_menuActive() override{
-        return true;
+        return (io.status() < AIO_CONNECTED);
     }
     void on_ButtonPress(int buttonIndex, Button* button) override{
-        Serial.println("Example 3 did button press");
+        io.run();
+        SliderPadTestValue->save(1);
     }
+    void run_menu() override{
+        io.run();
+    }
+
 };
